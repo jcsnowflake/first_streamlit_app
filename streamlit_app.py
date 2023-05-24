9a6c0f5b46acf2892aff69ebbbc3d1e62ce49df0
jcsnowflake <134535395+jcsnowflake@users.noreply.github.com>
2023-05-24 12:55:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner');
 
 streamlit.header("Breakfast Menu");
+
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Smothie');
+streamlittext('Hard-Boiled Free-Range Egg');
+
