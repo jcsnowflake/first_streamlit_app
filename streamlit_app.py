0954832ec982a9dce4a8b287b13c18a902e2d37e
jcsnowflake <134535395+jcsnowflake@users.noreply.github.com>
2023-05-24 13:56:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 
-my_fruit_list.set_index('Fruit')
+my_fruit_list.set_index('Fruit');
 
 streamlit.title('My Parents New Healthy Diner');
 
@@ -20,6 +20,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 streamlit.text('Index column =');
 streamlit.text(my_fruit_list.index);
 
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list.Fruit))
 
 streamlit.dataframe(my_fruit_list);
