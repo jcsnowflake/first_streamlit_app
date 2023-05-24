b1d9effdc6ed8fa2014955f579e710a2dcf92498
jcsnowflake <134535395+jcsnowflake@users.noreply.github.com>
2023-05-24 13:54:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 
-my_fruit_list.set_index('Fruit');
+my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner');
 
@@ -18,7 +18,7 @@ streamlit.text('🥑🍞 Avacado Toast');
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 # Let's put a pick list here so they can pick the fruit they want to include
 streamlit.text('Index column =');
-streamlit.text(my_fruit_list.get_index());
+streamlit.text(my_fruit_list.index);
 
 streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 
