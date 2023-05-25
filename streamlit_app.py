61cf5f1cea6b1ad8ec4ecdd574d69508d55779dd
jcsnowflake <134535395+jcsnowflake@users.noreply.github.com>
2023-05-25 13:21:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,13 @@ import pandas
 import  requests
 import snowflake.connector
 from urllib.error import URLError
-
+#create reusable functions
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice);
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+    return fruityvice_normalized;
+  
+  
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 
@@ -35,9 +41,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
-    streamlit.dataframe(fruityvice_normalized);
+    back_from_function = get_fruityvice_data(fruit_choice);
+    streamlit.dataframe(back_from_function);
 except URLError as e:
   streamlit.error();
   
