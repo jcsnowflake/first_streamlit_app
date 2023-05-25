729fe81b9e90dc592d2ab79e38e151c857e36ea0
jcsnowflake <134535395+jcsnowflake@users.noreply.github.com>
2023-05-25 13:50:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,11 +63,16 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list();
     streamlit.dataframe(my_data_rows);
 
+    
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')");
+        return "Thanks for adding " + new
+        
 streamlit.stop();
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit');
 streamlit.write('Thank you for adding ', add_my_fruit);  
 
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')");
+
                    
